46186d184aacd0441d23241be3ce790aaae3c6d8
thinkingalaud <thinkingalau@gmail.com>
2023-08-06 01:03:02 -0700

    update README and use more .env variables

--- README.md
+++ README.md
@@ -1,2 +1,22 @@
 # deal_watcher
 Watch webpages for price changes or out of stock changes
+
+# How it works
+* playwright-python opens a browser and navigates to your URL
+* checks whether the text string does or does not show up in the text of the CSS selector
+* hits the notification URL
+** IFTTT is used and json with a "value1" is provided, but any webhook that accepts a json payload is fine
+
+# Installation
+* install python 3
+* `pip install -r requirements.txt`
+* write a .env file with the parameters
+```
+URL=<webpage to watch>
+NOTIFICATION_URL=<webhook to hit when condition is satisfied>
+CSS_SELECTOR=<css selector for text to watch>
+TEXT=<text to search for>
+NOTIFY_WHEN_CONTAINS=<boolean, true for a notification when the text is present, false for a notification when the text is NOT present>
+```
+* set up `python watch_once.py` to run on a recurring basis (cron, etc)
+
--- watch_once.py
+++ watch_once.py
@@ -7,17 +7,20 @@ load_dotenv()
 
 url = environ['URL']
 notification_url = environ['NOTIFICATION_URL']
+css = environ['CSS_SELECTOR']
+text = environ['TEXT']
+notify_when_contains = environ['NOTIFY_WHEN_CONTAINS'] == 'true'
 
 with sync_playwright() as p:
   browser = p.chromium.launch(headless=False)
   page = browser.new_page()
   page.goto(url)
-  page.wait_for_selector('id=selling-essentials')
-  selling = page.locator('id=selling-essentials')
-  sold_out = "SOLD OUT" in selling.inner_text()
-  if !sold_out:
+  page.wait_for_selector(css)
+  element = page.locator(css)
+  contains = text in element.inner_text()
+  if (contains and notify_when_contains) or (not contains and not notify_when_contains):
     data = {
-      "value1": f"Nordstrom Uppababy Vista back in stock: {url}",
+      "value1": f"{url} {'contains' if contains else 'does not contain'} {text}",
     }
     requests.post(notification_url, json=data, headers={'Content-Type': 'application/json'})
     print("sent notification")
